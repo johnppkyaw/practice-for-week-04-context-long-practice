d4938c3dfeb6cbdde206b93e64a23d6102e8dcdb
John <jjpyae@gmail.com>
2023-05-10 16:22:50 -0700

    Uncommented local tests

--- problems/11-bind-set-time-out.js
+++ problems/11-bind-set-time-out.js
@@ -17,9 +17,9 @@ function boundFuncTimer(obj, func, delay) {
 // const dog = new Animal(1);
 // const cat = new Animal(5);
 
-boundFuncTimer(dog, dog.growOlder, 5000); // in 5 seconds prints: 2
-boundFuncTimer(cat, dog.growOlder, 7000); // in 7 seconds prints: 6
-/*****************************************************************************/
+// boundFuncTimer(dog, dog.growOlder, 5000); // in 5 seconds prints: 2
+// boundFuncTimer(cat, dog.growOlder, 7000); // in 7 seconds prints: 6
+// /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
 
 module.exports = boundFuncTimer;
