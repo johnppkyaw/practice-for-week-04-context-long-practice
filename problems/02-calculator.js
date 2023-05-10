57d44b876910e6be30711160b5a627609c081497
John <jjpyae@gmail.com>
2023-05-10 15:29:31 -0700

    Completed 1-5

--- problems/02-calculator.js
+++ problems/02-calculator.js
@@ -1,4 +1,24 @@
-// Your code here
+class Calculator {
+	constructor() {
+		this.total = 0;
+	}
+
+	add(num) {
+		return this.total += num;
+	}
+
+	subtract(num) {
+		return this.total -= num;
+	}
+
+	multiply(num) {
+		return this.total *= num;
+	}
+
+	divide(num) {
+		return this.total /= num;
+	}
+}
 
 
 /*****************************************************************************/
--- problems/04-change-context.js
+++ problems/04-change-context.js
@@ -1,6 +1,20 @@
 function changeContext(func, obj) {
-  // Your code here
+  return func.bind(obj)();
 }
+
+//local testing
+// class Person {
+//   constructor(name) {
+//     this.name = name;
+//   }
+// }
+
+// function extractName() {
+//   return this.name;
+// }
+
+// const kristen = new Person('Kristen');
+// console.log(changeContext(extractName, kristen)); // => Kristen
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
