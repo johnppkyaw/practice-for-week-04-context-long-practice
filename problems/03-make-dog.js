2bd2a28abfba745cea1202c92f5a92ce4db9034c
John <jjpyae@gmail.com>
2023-05-10 16:16:36 -0700

    All tests passed

--- problems/03-make-dog.js
+++ problems/03-make-dog.js
@@ -18,16 +18,16 @@ class Dog {
 }
 
 //local testing
-let dog1 = Dog.makeJet(); // returns an object
+// let dog1 = Dog.makeJet(); // returns an object
 
-console.log(dog1.name);                 // Jet
-console.log(dog1.speak("hello"));       // Jet says hello
-console.log(dog1.changeName("Freyja")); // Freyja
-console.log(dog1.name);                 // Freyja
-console.log(dog1.speak("hello"));       // Freyja says hello
+// console.log(dog1.name);                 // Jet
+// console.log(dog1.speak("hello"));       // Jet says hello
+// console.log(dog1.changeName("Freyja")); // Freyja
+// console.log(dog1.name);                 // Freyja
+// console.log(dog1.speak("hello"));       // Freyja says hello
 
-let dog2 = Dog.makeJet();
-console.log(dog2.name);                 // Jet
+// let dog2 = Dog.makeJet();
+// console.log(dog2.name);                 // Jet
 
 /*****************************************************************************/
 /***************** DO NOT MODIFY ANYTHING UNDER THIS LINE ********************/
